91f48f46a47db1ee522080d89b76fbe66ecd9089
matthieuescuder@gmail.com <matthieuescuder@gmail.com>
2023-11-01 15:52:16 +0000

    table test

--- models/mart/finance/finance_campagns_month.sql
+++ models/mart/finance/finance_campagns_month.sql
@@ -1,6 +1,6 @@
 SELECT
-  DATE_TRUNC (date_date,MONTH) AS date_month
-, SUM(ads_margin) AS ads_margin
-, SUM(avg_basket) AS avg_basket
-FROM {{ref("finance_campagns_days")}}
-GROUP BY date_month 
+DATE_TRUNC(date_date,MONTH) AS date_month,
+SUM(ads_margin) AS ads_margin,
+SUM(avg_basket) AS avg_basket
+FROM {{ref('finance_campagns_day')}}
+GROUP BY date_month
